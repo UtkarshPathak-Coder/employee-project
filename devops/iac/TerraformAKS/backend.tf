19ee7e9c51010b5c2e2fac195cfa275fa01744fa
Utkarsh Pathak <85300009+UtkarshPathak-Coder@users.noreply.github.com>
2024-07-08 15:15:57 +0530

    Update backend.tf

--- devops/iac/TerraformAKS/backend.tf
+++ devops/iac/TerraformAKS/backend.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name   = var.backend_resource_group
-    storage_account_name  = var.backend_storage_account
-    container_name        = var.backend_container_name
-    key                   = var.backend_key
-    subscription_id       = var.azure_subscription_id
-    tenant_id             = var.azure_tenant_id
+    resource_group_name   = 
+    storage_account_name  = "Sagar-Heera–VS-MPN"
+    container_name        = "akscontainer"
+    key                   = "terraform.tfstate"
+    subscription_id       = "14d51dca-be7f-45d5-83f7-bc5ba5a5cfcb"
+    tenant_id             = "fe2cf472-1615-4350-ae39-21441d02fbf1"
   }
 }
 
@@ -19,6 +19,6 @@ terraform {
 provider "azurerm" {
   features {}
   environment     = "public"
-  subscription_id = var.azure_subscription_id
-  tenant_id       = var.azure_tenant_id
+  subscription_id = "14d51dca-be7f-45d5-83f7-bc5ba5a5cfcb"
+  tenant_id       = "fe2cf472-1615-4350-ae39-21441d02fbf1"
 }
