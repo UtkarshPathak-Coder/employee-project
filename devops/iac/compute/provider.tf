5eb673660379099e7b0efe160b67d5aa7819809c
Utkarsh Pathak <utkarsh.pathak@optym.com>
2024-07-11 16:55:55 +0530

    provider.tf update

--- devops/iac/compute/provider.tf
+++ devops/iac/compute/provider.tf
@@ -1,13 +1,5 @@
 terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"  
-      version = "~> 2.13"  
-    }
-  }
-}
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
+  
 }
 
 provider "azurerm" {
